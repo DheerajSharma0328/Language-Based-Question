acd495e8e0880b3ce5439407f229736d4952a995
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-05-26 18:47:19 +0530

    print number from n ot 1

---
+++ 01. Basic level 1/print_number_from_n_to_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    
+    for(int i=a; i>=0; i--){
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
