9736806b67c94eac3e67d8fe9bb51c7868914af4
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-05-26 18:48:12 +0530

    print odd number

---
+++ 01. Basic level 1/print_odd_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    int b=0;
+    
+    for(int i=0; i<=a; i++){
+        
+        b = i%2;
+        
+        if(b != 0){
+            cout<<i<<endl;
+        }
+    }
+    
+    return 0;
+}
